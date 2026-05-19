dd6e916be834b925ce643c5ee1ac087e428e1ed7
Jorge-3322 <5061494@clases.edu.sv>
2026-05-18 20:33:56 -0600

    Este es el ejercicio 7
    En este se solicito dos números

---
+++ Mayor7.php
@@ -0,0 +1,13 @@
+<?php
+$Numero1 = "30";
+$Numero2 = "70";
+
+echo "<center>";
+echo "<h1>Numero mayor</h1>";
+if ($Numero1 > $Numero2) {
+    echo "El primer numero 30 es mayor";
+} else {
+    echo "El segundo numero 70 es mayor";
+}
+echo "</center>";
+?>
