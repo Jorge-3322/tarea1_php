7bcaae0244a3895262a3484f171a3557f1b2accb
Jorge-3322 <5061494@clases.edu.sv>
2026-05-18 20:53:58 -0600

    Este es el ejercicio 17
    En este ejercicio se hizo una compra para ver si obtenia un descuento del 10% pero solo si super una cierta cantidad

---
+++ Monto17.php
@@ -0,0 +1,10 @@
+<?php
+$Compra = "100";
+$Descuento = "10%";
+
+if ($Compra > $Descuento) {
+    echo "Su compra aplica para el descuento";
+} else {
+    echo "Su compra no aplica para el descuento";
+}
+?>
