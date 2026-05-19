20e9d3cc142999df597e417e2832ad89f9fc5eb7
Jorge-3322 <5061494@clases.edu.sv>
2026-05-18 20:32:50 -0600

    Este es el ejercicio 6
    En este se solicito el nombre y la edad del usuario

---
+++ Solicite6.php
@@ -0,0 +1,12 @@
+<?php
+$Nombre = $_POST['Nombre'];
+$Edad = $_POST['Edad'];
+
+echo "<center>";
+echo "<h1>Datos</h1>";
+echo "<br><br>";
+echo "Su Nombre es:" . $Nombre;
+echo "<br><br>";
+echo "Su Edad es:" . $Edad;
+echo "</center>";
+?>
