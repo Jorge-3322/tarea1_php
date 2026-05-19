bbdfdd59bd43efd8b7e7f149af68b4a97e2a754f
Jorge-3322 <5061494@clases.edu.sv>
2026-05-18 21:06:00 -0600

    Este es el ejercicio 25
    En este se solicito el nombre la ciudad, el correo electronico, y se tenia que mostrar los datos ya ingresados

---
+++ Datos25.php
@@ -0,0 +1,11 @@
+<?php
+$Nombre = $_POST['Nombre'];
+$ciudad = $_POST['ciudad'];
+$correo_electronico = $_POST['correo_electronico'];
+
+echo "Su nombre es:" . $Nombre;
+echo "<br><br>";
+echo "Su ciudad es:" . $ciudad;
+echo "<br><br>";
+echo "Su correo electronico es:" . $correo_electronico;
+?>
