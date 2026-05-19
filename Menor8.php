7a0519abd50b4c20dd2c8b298b2be3fa20700860
Jorge-3322 <5061494@clases.edu.sv>
2026-05-18 20:35:10 -0600

    Este es el ejercicio 8
    En este se solicito dos números para saber cual era el número menor

---
+++ Menor8.php
@@ -0,0 +1,13 @@
+<?php
+$Numero1 = "50";
+$Numero2 = "30";
+
+echo "<center>";
+echo "<h1>Numero menor</h1>";
+if ($Numero1 < $Numero2) {
+    echo "El primer numero 50 es mayor";
+} else {
+    echo "El segundo numero 30 es menor";
+}
+echo "</center>";
+?>
